ce51569ea8e71bd2bfc2735183f9289fdc09e180
tomo-kmr <tkkt0082@gmail.com>
2020-11-12 16:55:21 +0900

    エラー確認

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,28 +1,28 @@
-# module MarkdownHelper
-#   require 'redcarpet'
-#   require 'coderay'
+module MarkdownHelper
+  require 'redcarpet'
+  require 'coderay'
 
-#   REDCARPET_OPTIONS = {
-#     autolink: true,
-#     space_after_headers: true,
-#     no_intra_emphasis: true,
-#     fenced_code_blocks: true,
-#     tables: true,
-#     hard_wrap: true,
-#     xhtml: true,
-#     lax_html_blocks: true,
-#     strikethrough: true
-#   }.freeze
+  REDCARPET_OPTIONS = {
+    autolink: true,
+    space_after_headers: true,
+    no_intra_emphasis: true,
+    fenced_code_blocks: true,
+    tables: true,
+    hard_wrap: true,
+    xhtml: true,
+    lax_html_blocks: true,
+    strikethrough: true
+  }.freeze
 
-#   def markdown_to_html(md_code)
-#     html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
-#     markdown = Redcarpet::Markdown.new(html_render, REDCARPET_OPTIONS)
-#     markdown.render(md_code)
-#   end
+  def markdown_to_html(md_code)
+    html_render = HTMLwithCoderay.new(filter_html: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(html_render, REDCARPET_OPTIONS)
+    markdown.render(md_code)
+  end
 
-#   class HTMLwithCoderay < Redcarpet::Render::HTML
-#     def block_code(code, language)
-#       CodeRay.scan(code, language || 'md').div
-#     end
-#   end
-# end
+  class HTMLwithCoderay < Redcarpet::Render::HTML
+    def block_code(code, language)
+      CodeRay.scan(code, language || 'md').div
+    end
+  end
+end
